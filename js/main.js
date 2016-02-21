f0509fdf7fd0929fbfd888a2e6b16364abfd9803
Anoop <optimistanoop@gmail.com>
2016-02-21 13:37:46 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,90 @@
+/*
+ * Turn.js responsive book
+ */
+
+/*globals window, document, $*/
+
+(function () {
+    'use strict';
+
+    var module = {
+        ratio: 1.38,
+        init: function (id) {
+            var me = this;
+
+            // if older browser then don't run javascript
+            if (document.addEventListener) {
+                this.el = document.getElementById(id);
+                this.resize();
+                this.plugins();
+
+                // on window resize, update the plugin size
+                window.addEventListener('resize', function (e) {
+                    var size = me.resize();
+                    $(me.el).turn('size', size.width, size.height);
+                });
+            }
+        },
+        resize: function () {
+            // reset the width and height to the css defaults
+            this.el.style.width = '';
+            this.el.style.height = '';
+
+            var width = this.el.clientWidth,
+                height = Math.round(width / this.ratio),
+                padded = Math.round(document.body.clientHeight * 0.9);
+
+            // if the height is too big for the window, constrain it
+            if (height > padded) {
+                height = padded;
+                width = Math.round(height * this.ratio);
+            }
+
+            // set the width and height matching the aspect ratio
+            this.el.style.width = width + 'px';
+            this.el.style.height = height + 'px';
+
+            return {
+                width: width,
+                height: height
+            };
+        },
+        plugins: function () {
+            // run the plugin
+            $(this.el).turn({
+                gradients: true,
+                acceleration: true
+            });
+            // hide the body overflow
+            document.body.className = 'hide-overflow';
+        }
+    };
+
+    module.init('book');
+// flipbook-controls
+$(".prev").click(function(e){
+    e.preventDefault();
+    $('.book').turn("previous");
+});
+
+$(".next").click(function(e){
+    e.preventDefault();
+    $('.book').turn("next");
+});
+$('.submit-btn').click(function(){
+    var firstName = $('#firstName').val();
+    var lastName = $('#lastName').val();
+    var gender = $("input[name='gender']:checked").val();
+    var dob = $('#dob').val();
+    var profession = $('#profession').val();
+    var summary = $('#summary').val();
+    $('#firstNameLabel').text(firstName);
+    $('#lastNameLabel').text(lastName);
+    $('#genderLabel').text(gender);
+    $('#dobLabel').text(dob);
+    $('#professionLabel').text(profession);
+    $('#summaryLabel').text(summary);
+});
+
+}());
+
